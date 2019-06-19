dde455f1a20ea9f385dd6b46cb025f8daeba6d30
Pedro Xavier <48462107+pedrohsxavier@users.noreply.github.com>
2019-06-18 21:45:40 -0300

    sql answers

--- SQL-Answers/2618 - Produtos Importados.sql
+++ SQL-Answers/2618 - Produtos Importados.sql
@@ -1,9 +1,5 @@
 SELECT pr.name, p.name, c.name FROM products pr JOIN providers p
-
 ON p.id = pr.id_providers
-
 JOIN categories c
-
 ON c.id = pr.id_categories
-
 WHERE p.name = 'Sansul SA' AND c.name = 'Imported'
