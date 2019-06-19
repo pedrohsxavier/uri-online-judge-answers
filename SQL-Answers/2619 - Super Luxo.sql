ba289719214a91ae4f7c4e0d89700b3877e70738
Pedro Xavier <48462107+pedrohsxavier@users.noreply.github.com>
2019-06-18 21:46:02 -0300

    sql answers

--- SQL-Answers/2619 - Super Luxo.sql
+++ SQL-Answers/2619 - Super Luxo.sql
@@ -1,9 +1,5 @@
 SELECT pr.name, p.name, pr.price FROM products pr JOIN providers p
-
 ON p.id = pr.id_providers
-
 JOIN categories c
-
 ON c.id = pr.id_categories
-
 WHERE pr.price > 1000 AND c.name = 'Super Luxury'
