974f6acf4bdd7268091e1974d1f2575bb587fe34
pedrohsxavier <pedro.hsxavier@outlook.com>
2019-08-17 00:02:37 -0300

    2766

---
+++ Python-Answers/Iniciante/2766.py
@@ -0,0 +1,17 @@
+names = list(range(10))
+
+names[0] = str(input())
+names[1] = str(input())
+names[2] = str(input())
+names[3] = str(input())
+names[4] = str(input())
+names[5] = str(input())
+names[6] = str(input())
+names[7] = str(input())
+names[8] = str(input())
+names[9] = str(input())
+
+
+print(names[2])
+print(names[6])
+print(names[8])
